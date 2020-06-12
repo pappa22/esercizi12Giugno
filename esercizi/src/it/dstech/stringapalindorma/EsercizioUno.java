98879aa1b0c4c03a4f694f24abbb5ccc57174bd5
pappa22 <marta.yiannakou@gmail.com>
2020-06-12 14:15:20 +0200

    esercizio stringa palindorma completato

---
+++ esercizi/src/it/dstech/stringapalindorma/EsercizioUno.java
@@ -0,0 +1,29 @@
+package it.dstech.stringapalindorma;
+
+import java.util.Scanner;
+
+public class EsercizioUno {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String stringa = new String();
+	
+		do {
+			System.out.println("Inserisci una parola");
+			stringa = scanner.nextLine();
+			stringa = stringa.replaceAll("\\s", "");
+			String reverse = "";
+			 int length = stringa.length();
+		    for (int i = length - 1; i >= 0; i--)
+			      reverse = reverse + stringa.charAt(i);
+
+			    if (stringa.equals(reverse)) {
+				System.out.println("La parola è palindroma");
+			}
+			else {
+				System.out.println("La parola NON è palindroma");
+			}
+			    System.out.println("Vuoi continuare? Scrivi sì");
+		} while (scanner.nextLine().equals("sì")); {
+	} 
+	}
+}
